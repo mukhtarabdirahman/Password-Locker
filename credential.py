317d5a45908f3ae61be2525740df751a9779aed9
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2019-12-14 21:31:20 +0300

    credential

---
+++ credential.py
@@ -0,0 +1,10 @@
+class Credential:
+    '''
+    Class to create  account credentials, generate passwords and save their information
+    '''
+    credentials_list = []
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+    
+    
