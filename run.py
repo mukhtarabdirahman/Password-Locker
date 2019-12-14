c94ebb93a88692ba7cd49f135c072f58a02930c5
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2019-12-14 19:24:59 +0300

    fuction to creat a user

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3.6
+from user import User
+
+def create_user(username,password):
+    """
+    Function to create a user
+    """
+    new_user = User(username,password)
+    return new_user
+
