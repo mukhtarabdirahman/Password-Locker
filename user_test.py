231c9d5d942824bb7b5220831149a65baf9a62dd
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2019-12-14 16:00:37 +0300

    def setUp

--- user_test.py
+++ user_test.py
@@ -1,1 +1,12 @@
 import unittest # Importing the unittest module
+from user import User # Importing the User
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        """
+        Set up method to run before each test cases.
+        """
+    
+    
+if __name__ == '__main__':
+    unittest.main()
