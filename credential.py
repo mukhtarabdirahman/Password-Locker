c4d82ff7c17e97fcd671fe2297486f797a9a8774
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2019-12-14 21:58:08 +0300

    test save credential

--- credential.py
+++ credential.py
@@ -6,6 +6,11 @@ class Credential:
     def __init__(self,username,password):
         self.username = username
         self.password = password
-    
+    def save_credential(self):
+        """
+        save_user method saves credential objects into user_list
+        """
+        Credential.credentials_list.append(self)
+        
     
     
--- credential_test.py
+++ credential_test.py
@@ -7,4 +7,18 @@ class TestCredential(unittest.TestCase):
         Set up method to run before each test cases.
         """
         self.new_credential = Credential ('mukhtarabdirahman', 'codePanther') # created credential object
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_credential.username,'mukhtarabdirahman') 
+        self.assertEqual(self.new_credential.password,'codePanther') 
+    def test_save_credential(self):
+        """
+        test_save_credential test case to test if the contact object is saved into
+        the credential list
+        """
+        self.new_credential.save_credential()# saving the new credential
+if __name__ == '__main__':
+    unittest.main()
         
