23184a6fe4943fae0b40432f2a6eb46dd77f685f
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2019-12-14 19:30:41 +0300

    function to save user

--- run.py
+++ run.py
@@ -7,4 +7,12 @@ def create_user(username,password):
     """
     new_user = User(username,password)
     return new_user
-
+def save_user(user):
+    """
+    Function to save user
+    """
+    
+    user.save_user()
+    
+    
+    
