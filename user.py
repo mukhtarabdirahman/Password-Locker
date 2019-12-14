71f8af02323a65d1e7f4c5ddd155d14a785b9703
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2019-12-14 15:07:18 +0300

    class user

---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    """
+    Class that generates new instances user
+    """
+    
+    user_list = []
+    
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        
+    
+    
